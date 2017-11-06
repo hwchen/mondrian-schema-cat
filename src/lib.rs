bd908cfb6e743d6a992dbfe0c54b03d031b7c834
Walther Chen <walther.chen@gmail.com>
2017-11-05 20:40:17 -0500

    get_shared_dims implemented

--- src/lib.rs
+++ src/lib.rs
@@ -37,16 +37,18 @@ use error::*;
 
 // I assume tags follow the convention of CamelCase
 const SCHEMA_TAG_OPEN: &str = r#"<Schema name=""#;
+const SCHEMA_TAG_CLOSE: &str = r#"</Schema>"#;
 const CUBE_TAG_OPEN: &str = "<Cube";
 const CUBE_TAG_CLOSE: &str = "</Cube>";
 const DIM_TAG_OPEN: &str = "<Dimension";
 const DIM_TAG_CLOSE: &str = "</Dimension>";
 
 
-
+/// Struct to hold the results of parsing
+/// a string fragment of schema.
 pub struct Fragment<'a> {
     schema_name: Option<&'a str>,
-    shared_dims: Vec<&'a str>,
+    shared_dims: Option<&'a str>,
     cubes: Vec<&'a str>,
 }
 
@@ -70,9 +72,50 @@ impl<'a> Fragment<'a> {
 
     }
 
-    // Get shared dims from one fragment
-    fn get_shared_dims(fragment: &'a str) -> Result<Vec<&'a str>> {
-        Ok(vec![])
+    /// Get shared dims from one fragment
+    fn get_shared_dims(fragment: &'a str) -> Option<&'a str> {
+        // Return everything between first Dim tag and first
+        // Cube tag.
+        //
+        // To make sure that don't get internal dims which
+        // happen to have a Cube tag after, need to make
+        // sure that there isn't a Cube tag before the
+        // dim tag.
+
+        if let Some(first_cube_idx) = fragment.find(CUBE_TAG_OPEN) {
+
+            // if there's a cube tag, get from dim_open_tag to cube tag,
+            // as long as it's not an internal dim.
+            fragment
+                .find(DIM_TAG_OPEN)
+                .and_then(|i| {
+                    fragment[i..]
+                        .find(CUBE_TAG_OPEN)
+                        .and_then(|j| {
+                            println!("{}, {}, {}", i, j, first_cube_idx);
+                            // if this cube tag idx is the
+                            // first cube idx, then it means that
+                            // the dims are shared, so get frag.
+                            // Otherwise it's an internal  dim.
+                            if i+j == first_cube_idx {
+                                fragment.get(i..i+j)
+                            } else {
+                                None
+                            }
+                        })
+                })
+        } else {
+
+            // if there's no cube tag, then get from dim_open_tag to end
+            fragment.find(DIM_TAG_OPEN)
+                .and_then(|i| {
+                    fragment[i..]
+                        .find(SCHEMA_TAG_CLOSE).or(Some(fragment.len()-i))
+                        .and_then(|j| {
+                            fragment.get(i..i+j)
+                        })
+                })
+        }
     }
 
     // Get cubes from one fragment
@@ -87,7 +130,7 @@ impl<'a> Fragment<'a> {
     // Get shared Dim names from
         Fragment {
             schema_name: None,
-            shared_dims: vec![],
+            shared_dims: None,
             cubes: vec![],
         }
     }
@@ -104,5 +147,47 @@ mod tests {
         assert_eq!(Fragment::get_schema_name(fragment), Some("testname"));
         let fragment = r#"<Cube name="testname"></Cube>"#;
         assert_eq!(Fragment::get_schema_name(fragment), None);
+    }
+    #[test]
+    fn test_get_share_dims() {
+        let fragment = r#"<Schema name="testname">
+            <Cube name="testcube"></Cube></Schema>"#;
+        assert_eq!(Fragment::get_shared_dims(fragment), None);
+
+        // gets shared dims before cubes
+        let fragment = r#"<Schema name="testname">
+            <Dimension></Dimension><Cube name="testcube"></Cube></Schema>"#;
+        assert_eq!(
+            Fragment::get_shared_dims(fragment),
+            Some("<Dimension></Dimension>")
+        );
+
+        // does not get internal dims within cube
+        // (this test has an extra Cube to make sure
+        // that adding a Cube tag match after the dim
+        // in this case doesn't trigger getting the
+        // intermal dim
+        let fragment = r#"<Schema name="testname">
+            <Cube name="testcube"><Dimension></Dimension></Cube>
+            <Cube name="a"></Cube>
+            </Schema>"#;
+        assert_eq!(
+            Fragment::get_shared_dims(fragment),
+            None
+        );
+
+        // Test only shared dims, both with and without schema tag
+        let fragment = r#"<Schema name="test">
+            <Dimension name="a"></Dimension></Schema>"#;
+        assert_eq!(
+            Fragment::get_shared_dims(fragment),
+            Some(r#"<Dimension name="a"></Dimension>"#)
+        );
+
+        let fragment = r#"<Dimension name="a"></Dimension>"#;
+        assert_eq!(
+            Fragment::get_shared_dims(fragment),
+            Some(r#"<Dimension name="a"></Dimension>"#)
+        );
     }
 }
